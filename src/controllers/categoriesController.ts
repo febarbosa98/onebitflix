3c34401bbef8405552a11f9f6beaa9ef98fc9485
Fernando Barbosa <fernando_fe1@hotmail.com>
2024-08-22 14:49:13 -0300

    obetendo informaçoes do curso

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express";
-import { Category } from "../models";
+import { categoryService } from "../services/categoryService";
+import { getpaginationParams } from "../helpers/getpaginationParams";
 
 export const categoriesCOntroller = {
+    // GET /categories
     index: async (req: Request, res: Response) => {
-
+        const [page, perPage] = getpaginationParams(req.query)
+		
         try {
-            const categories = await Category.findAll({
-                attributes: ['id', 'name', 'position'],
-                order: [['position', 'ASC']]
-            })
-            return res.json(categories)
+            const paginatedCategories = await categoryService.findAllPaginated(page, perPage)
+
+            return res.json(paginatedCategories)
         } catch (error) {
             if(error instanceof Error){
                 return res.status(400).json({message: error.message})
@@ -17,5 +18,18 @@ export const categoriesCOntroller = {
         }
         
         
+    },
+
+    // GET /categories/:id
+    show: async (req: Request, res: Response) =>{
+        const { id } = req.params
+        try {
+            const category = await categoryService.findByIdWithCourses(id)
+            return res.json(category)
+        } catch (err) {
+            if (err instanceof Error)
+            return res.status(400).json({message: err.message})
+        }
+
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import  express from "express"
 import { categoriesCOntroller } from "./controllers/categoriesController"
+import { coursesController } from "./controllers/coursesController"
 
 const router = express.Router()
 
 router.get('/categories', categoriesCOntroller.index)
+router.get('/categories/:id', categoriesCOntroller.show)
+
+router.get('/courses/:id', coursesController.show)
 
 export {router}
