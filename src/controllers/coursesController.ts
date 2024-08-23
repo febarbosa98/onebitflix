3dabd568a371bc7cd8bd0a91e41b4302a8a406c3
Fernando Barbosa <fernando_fe1@hotmail.com>
2024-08-23 01:04:20 -0300

    adicionado mais rota

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { coursesService } from "../services/coursesServoce"
+import { getpaginationParams } from "../helpers/getpaginationParams"
 
 export const coursesController = {
     // GET /courses/featured
@@ -8,6 +9,33 @@ export const coursesController = {
             const featuredCourses = await coursesService.getRandomFeaturedCourses()
             return res.json(featuredCourses)
 
+        } catch (err) {
+            if(err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+    // GET /courses/newest
+    newest: async (req: Request, res: Response) => {
+        try {
+            const newestCourses = await coursesService.getTopTenNewest()
+            return res.json(newestCourses)
+
+        } catch (err) {
+            if(err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+       // GET /courses/search?name=
+       search: async (req: Request, res: Response) => {
+        const {name} = req.query
+        const [page, perPage] = getpaginationParams(req.query)
+        try {
+            if (typeof name !== 'string') throw new Error('name param must be of type string')
+            const courses = await coursesService.findByName(name, page, perPage)
+
+            return res.json(courses)
         } catch (err) {
             if(err instanceof Error){
                 return res.status(400).json({message: err.message})
