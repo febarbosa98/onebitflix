471c08cc9e164e0d4e4a2864ee2d2154508ca690
Fernando Barbosa <fernando_fe1@hotmail.com>
2024-09-02 20:29:39 -0300

    token no video

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from "express";
+import { JwtPayload } from "jsonwebtoken";
+import { UserInstance } from "../models/User";
+import { jwtService } from "../services/jwtService";
+import { userService } from "../services/userService";
+
+export interface authenticatedRequest extends Request{
+    user?: UserInstance | null
+}
+
+export function ensureAuth(req: authenticatedRequest, res: Response, next: NextFunction) {
+    const authorizationHeader = req.headers.authorization
+
+    if(!authorizationHeader) return res.status(401).json({
+        message: "Não autorizado: nenhum token foi encontrado"
+    })
+
+    const token = authorizationHeader.replace(/Bearer /, '')
+
+    jwtService.verifyToken(token, async (err, decoded) => {
+        if(err || typeof decoded === 'undefined') return res.status(401).json({
+            message: 'Não autorizado: token invalido'
+        })
+
+      const user = await userService.findByEmail((decoded as JwtPayload).email)
+            req.user = user
+            next()
+        })
+    
+}
+
+export function ensureAuthViaQuery(req: authenticatedRequest, res: Response, next: NextFunction) {
+    const {token} = req.query
+
+    if(!token) return res.status(401).json({
+        message: "Não autorizado: nenhum token foi encontrado"
+    })
+
+    if(typeof token !== 'string') return res.status(400).json({
+        message: "O parametro token deve ser do tipo string"
+    })
+    
+    jwtService.verifyToken(token, async (err, decoded) => {
+        if(err || typeof decoded === 'undefined') return res.status(401).json({
+            message: 'Não autorizado: token invalido'
+        })
+
+        
+       const user = await userService.findByEmail((decoded as JwtPayload).email)
+            req.user = user
+            next()
+        })
+    
+    
+}
--- src/routes.ts
+++ src/routes.ts
@@ -3,20 +3,21 @@ import { categoriesCOntroller } from "./controllers/categoriesController"
 import { coursesController } from "./controllers/coursesController"
 import { episodeController } from "./controllers/episodeController"
 import { authController } from "./controllers/authController"
+import { ensureAuth, ensureAuthViaQuery } from "./middlewares/auth"
 
 const router = express.Router()
 
 router.post('/auth/register', authController.register)
 router.post('/auth/login', authController.login)
 
-router.get('/categories', categoriesCOntroller.index)
-router.get('/categories/:id', categoriesCOntroller.show)
+router.get('/categories',ensureAuth, categoriesCOntroller.index)
+router.get('/categories/:id', ensureAuth, categoriesCOntroller.show)
 
-router.get('/courses/featured', coursesController.featured)
+router.get('/courses/featured', ensureAuth, coursesController.featured)
 router.get('/courses/newest', coursesController.newest)
-router.get('/courses/search', coursesController.search)
-router.get('/courses/:id', coursesController.show)
+router.get('/courses/search', ensureAuth, coursesController.search)
+router.get('/courses/:id', ensureAuth, coursesController.show)
 
-router.get('/episodes/stream', episodeController.stream)
+router.get('/episodes/stream', ensureAuthViaQuery, episodeController.stream)
 
 export {router}
